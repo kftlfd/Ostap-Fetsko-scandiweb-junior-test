c2f522c28627645e5289e30de479bad634f1de3e
kftlfd <kftlfd@ex.ua>
2022-11-11 12:59:37 +0200

    change apache config

--- backend/dist/api.php
+++ backend/dist/api.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($className) {
-  $baseDir = __DIR__ . "/../lib/";
+  $baseDir = __DIR__ . "/lib/";
   $file = $baseDir . str_replace("\\", DIRECTORY_SEPARATOR, $className) . ".php";
   if (file_exists($file)) require $file;
 });
