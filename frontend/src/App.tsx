a45e535098e21b382381e46fb1871d3b269c16c7
kftlfd <kftlfd@ex.ua>
2022-11-18 19:57:47 +0200

    make router a separate component for useNavigate hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,33 @@
-import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  Link,
+} from "react-router-dom";
 import List from "./List";
 import Add from "./Add";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<List />} />
-        <Route path="/index.html" element={<Navigate to="/" />} />
-        <Route path="/add-product" element={<Add />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-
+      <Router />
       <footer>Scandiweb Test Assignment</footer>
     </BrowserRouter>
+  );
+}
+
+function Router() {
+  const navigate = useNavigate();
+
+  return (
+    <Routes>
+      <Route path="/" element={<List />} />
+      <Route path="/index.html" element={<Navigate to="/" />} />
+      <Route path="/add-product" element={<Add navigate={navigate} />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
@@ -23,9 +37,10 @@ function Error() {
       <header>
         <h1 className="heading">Not found</h1>
       </header>
+
       <main>
         <h3>
-          <Link to="/">Go home</Link>
+          <Link to="/">To Homepage</Link>
         </h3>
       </main>
     </>
