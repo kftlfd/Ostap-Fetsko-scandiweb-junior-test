8682a1200b1f740efbb729f85f3d54e5234e049c
kftlfd <kftlfd@ex.ua>
2022-12-01 14:16:09 +0200

    move to api v2

--- backend/api.php
+++ backend/api.php
@@ -5,5 +5,6 @@ require __DIR__ . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/config", "db.env");
 $dotenv->load();
 
-$api = new src\APIv1\API();
+// $api = new src\APIv1\API();
+$api = new src\APIv2\API();
 $api->processRequest();
